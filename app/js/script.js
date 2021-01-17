18fc1cad201a290bdf8bba3b51d87af7e79f14bf
domi <dominika.hustinova@kiwi.com>
2021-01-17 02:24:13 +0100

    feat: add toggling prices functionality

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,32 @@
 const toggleButton = document.querySelector("#toggleBtn");
+const priceBasic = document.querySelector(".basic .pricing__plan-price-amount");
+const priceProfessional = document.querySelector(
+  ".professional .pricing__plan-price-amount"
+);
+const priceMaster = document.querySelector(
+  ".master .pricing__plan-price-amount"
+);
 
 toggleButton.addEventListener("click", () => {
   if (toggleButton.classList.contains("monthly")) {
     toggleButton.classList.add("annualy");
     toggleButton.classList.remove("monthly");
+    showAnnuallyPrices();
   } else {
     toggleButton.classList.remove("annualy");
     toggleButton.classList.add("monthly");
+    showMonthlyPrices();
   }
 });
+
+function showMonthlyPrices() {
+  priceBasic.textContent = "19.99";
+  priceProfessional.textContent = "24.99";
+  priceMaster.textContent = "39.99";
+}
+
+function showAnnuallyPrices() {
+  priceBasic.textContent = "199.99";
+  priceProfessional.textContent = "249.99";
+  priceMaster.textContent = "399.99";
+}
